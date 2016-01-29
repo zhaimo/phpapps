71f7df79a9f844aff9d29fdd411a2f181a37b37d
zhaimo <zhaimo@yahoo.com>
2016-01-29 11:57:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                 <h1>Hello Albert</h1>
                 <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
                 <div align="center"><img src="https://s3.amazonaws.com/albertpic001/IMG_0548.JPG"></div>
-                <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>You Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
     </body>
